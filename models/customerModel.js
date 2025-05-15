95c98fb176fb6d63034fb3a637984ae3fd9f808b
Steven Nataniel <stevnathar@gmail.com>
2025-05-15 22:21:18 +0700

    Update customerModel.js

--- models/customerModel.js
+++ models/customerModel.js
@@ -1,19 +1,19 @@
 const { execute } = require('../config/db');
 
 const Customer = {
-  create: async (nama, pengirim, jenis_pesanan) => {
+  create: async (nama, pengirim, jenis_pesanan, tipe_ambil) => {
     const insertQuery = `
       INSERT INTO customer (nama, pengirim, jenis_pesanan)
       VALUES (?, ?, ?)
     `;
 
-    const result = await execute(insertQuery, [nama, pengirim, jenis_pesanan]);
-    return result.insertId; // return idcustomer
+    const result = await execute(insertQuery, [nama, pengirim, jenis_pesanan, tipe_ambil]);
+    return result.insertId; 
   },
 
   getAll: async () => {
     const query = `
-      SELECT idcustomer, nama, pengirim, jenis_pesanan
+      SELECT idcustomer, nama, pengirim, jenis_pesanan, tipe_ambil
       FROM customer
     `;
     return await execute(query);
